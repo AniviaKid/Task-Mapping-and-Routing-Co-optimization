fix taskgraph,update reward calculation

libs.py
@@ -79,11 +79,21 @@ def Get_Neighborhood(position,radius,M,N): #return a list which consists of posi
                     neighborhood.append(i*N+j)
     return neighborhood
 
-def Get_mapping_reward(PEs_task_current_solution,computation_ability,M,N):
+def Get_mapping_exe_time(PEs_task_current_solution,Tasks_position_current_solution,computation_ability,num_of_rows,execution):
+    num_of_tasks=len(execution)-1
+    ret_execution=copy.deepcopy(execution)
+    for i in Tasks_position_current_solution.keys():#首先计算是否有task被匹配到同一个PE，有的话添加惩罚，但是如果是这同一个PE上有任务图中同一条边上的两个task，意思就是它们一定不同时运行，这个时候不计算惩罚
+        position=Tasks_position_current_solution[i]
+        if(len(PEs_task_current_solution[position])>1):
+            ret_execution[i+1]=ret_execution[i+1]*len(PEs_task_current_solution[position])
+
+    #然后根据运行能力倍减运行时间
+    for i in Tasks_position_current_solution.keys():
+        position=Tasks_position_current_solution[i]
+        ret_execution[i+1]=int(ret_execution[i+1]/computation_ability[int(position/num_of_rows)][position%num_of_rows])
     ret=0
-    for i in range(0,len(PEs_task_current_solution)):
-        if(len(PEs_task_current_solution[i])): #this PE has tasks
-            ret+=computation_ability[int(i/N)][i%N]
+    for i in ret_execution:
+        ret+=i
     return ret
 
 def Get_detailed_data(num_of_tasks,edges,comp_cost):#输出邻接矩阵,total_needSend,total_needReceive,execution
@@ -190,8 +200,7 @@ def Get_full_route_by_XY(part_route,source_position,dest_position,num_of_rows):
     return ret
     
 def Get_reward_by_pendTimes(pendTimes):
-    #pendTimes<=7时才能得到非负的reward
-    return 7-pendTimes
+    return 0-pendTimes
 
 
 #state为[state_tensor,cur_position,partRouteFromRL]，传进来的partRoute的格式是直接的路由表，没有第一位第二位的task
@@ -384,6 +393,13 @@ class Critic(nn.Module):
 
 def Get_rand_computation_ability(num_of_rows):
     ret=np.random.randint(1,5,(num_of_rows,num_of_rows))
+    return ret
+
+def Get_rand_computation_ability2(num_of_rows):
+    ret=np.full((num_of_rows,num_of_rows),0.5)
+    for i in range(0,num_of_rows*num_of_rows):
+        random_choose=np.random.randint(0,num_of_rows*num_of_rows)
+        ret[int(random_choose/num_of_rows)][int(random_choose%num_of_rows)]+=0.5
     return ret
 
 if __name__ == '__main__':

main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import json
-from libs import init,Get_Neighborhood,Get_mapping_reward,Get_rand_computation_ability,Get_detailed_data
+from libs import init,Get_Neighborhood,Get_mapping_exe_time,Get_detailed_data,Get_rand_computation_ability2
 import copy
 from routing import routeCompute
 import time
@@ -13,13 +13,13 @@ computation_ability=np.array([
  [3, 3, 1, 2],
  [1, 3, 2, 2]])
  """
-computation_ability=Get_rand_computation_ability(num_of_rows=8)#2的指数级，4/8/16
+computation_ability=Get_rand_computation_ability2(num_of_rows=8)#2的指数级，4/8/16
 
 M=computation_ability.shape[0]
 N=computation_ability.shape[1]
 
 #init input
-hyperperiod,num_of_tasks,edges,comp_cost=init('./task graph/N12_autocor.tgff')
+hyperperiod,num_of_tasks,edges,comp_cost=init('./task graph/N12_autocor_AIR1.tgff')
 adj_matrix,total_needSend,total_needReceive,execution=Get_detailed_data(num_of_tasks,edges,comp_cost)
 
 
@@ -35,7 +35,7 @@ Tasks_position_current_solution={}#key-value: key=task, value=position in mesh
 PEs_task_best_solution=copy.deepcopy(PEs_task_current_solution)
 Tasks_position_best_solution=copy.deepcopy(Tasks_position_current_solution)
 best_task_graph={}
-Best_reward=-999999999
+Best_reward=999999999
 
 Tabu_list=[]
 Tabu_length=2
@@ -64,22 +64,11 @@ def Update_tabu_list(choosen_position, target_position):
 
 
 def Iteration(num_of_tasks,radius,num_of_rows): #expand neighborhood, find the fittest, update best solution and tabu list
-    """
-    print("Current Sol:")
-    print(PEs_task_current_solution)
-    print(Tasks_position_current_solution)
-    print("------------")
-    print("Current reward=",Get_mapping_reward(PEs_task_current_solution,computation_ability,M,N))
-    print("------------")
-    """
     randomly_selected_task=np.random.randint(0,num_of_tasks) #randomly choose a task
-    #print("Randomly choose:"," task ",randomly_selected_task)
     current_position=Tasks_position_current_solution[randomly_selected_task]
     neighborhood=Get_Neighborhood(current_position,radius,M,N)
-    #print("Full Neighborhood:",neighborhood)
-    #print("------------")
     target_position=-1
-    tmp_reward=-999999999
+    tmp_reward=999999999
     cur_task_graph={}
     for i in neighborhood: #visit all neighborhood and find the fittest one in these neighborhoods
         print("neighborhood:",i)
@@ -100,32 +89,26 @@ def Iteration(num_of_tasks,radius,num_of_rows): #expand neighborhood, find the f
         tmp_solution=copy.deepcopy(PEs_task_current_solution)
         tmp_solution[current_position].remove(randomly_selected_task)
         tmp_solution[i].append(randomly_selected_task)
-        reward=Get_mapping_reward(tmp_solution,computation_ability,M,N)
-        reward=reward*10#mapping reward的数量级与routing的相差过大
-        #print("mapping_reward:",reward)
 
         tmp_mapresults=copy.deepcopy(Tasks_position_current_solution)
         tmp_mapresults.update({randomly_selected_task:i})
+
+        mapping_exe_time=Get_mapping_exe_time(PEs_task_current_solution=tmp_solution,Tasks_position_current_solution=tmp_mapresults,computation_ability=computation_ability,num_of_rows=num_of_rows,execution=execution)
+
+
         tmp_mapresults1=[-1]#把task编号改成从1开始，然后传给routing部分
         for j in range(0,num_of_tasks):
             tmp_mapresults1.append(tmp_mapresults[j])
-        #print("tmp_map_result=",tmp_mapresults1)
         #通过RL计算最佳routing，然后获得routing的reward
         #首先根据MapResult更新execution
         execution_to_routing=copy.deepcopy(execution)
-        #print("exe:",execution_to_routing)
-        #print("map:",tmp_mapresults1)
         for j in range(1,num_of_tasks+1):
-            #print("j=",j)
             execution_to_routing[j]=int(execution_to_routing[j]/computation_ability[int(tmp_mapresults1[j]/num_of_rows)][tmp_mapresults1[j]%num_of_rows])
-        routing_reward,ret_task_graph=routeCompute(adj_matrix,num_of_tasks,execution_to_routing,num_of_rows,tmp_mapresults1)
-        #print(ret_task_graph)
-        #print("routing_reward:",routing_reward)
-        reward+=routing_reward
-        #print("reward=",reward)
+        pendTimes,ret_task_graph=routeCompute(adj_matrix,num_of_tasks,execution_to_routing,num_of_rows,tmp_mapresults1)
+        total=mapping_exe_time+pendTimes#这个值应该是越小越好，第一个值小表示mapping结果匹配到的PE计算能力强，第二个值小表示routing中争用少
 
-        if(reward>tmp_reward):
-            tmp_reward=reward
+        if(total<tmp_reward):
+            tmp_reward=total
             target_position=i
             cur_task_graph=copy.deepcopy(ret_task_graph)
     
@@ -133,13 +116,11 @@ def Iteration(num_of_tasks,radius,num_of_rows): #expand neighborhood, find the f
     PEs_task_current_solution[current_position].remove(randomly_selected_task)
     PEs_task_current_solution[target_position].append(randomly_selected_task)
     Tasks_position_current_solution.update({randomly_selected_task:target_position})
-    #print("target position:",target_position)
-    #print("------------")
     print("reward in this iteration is ",tmp_reward)
   
     #update the best solution
     global Best_reward
-    if(tmp_reward>Best_reward):
+    if(tmp_reward<Best_reward):#越小越好，详见line 108行注释
         #print("Update Best sol")
         Best_reward=tmp_reward
         global PEs_task_best_solution
@@ -151,8 +132,6 @@ def Iteration(num_of_tasks,radius,num_of_rows): #expand neighborhood, find the f
 
 
     Update_tabu_list(current_position,target_position)
-    #print("Tabu_list:",Tabu_list)
-    #print("------------")
         
 
 

routing.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 from torch.distributions import Categorical
 
 import matplotlib.pyplot as plt
-from libs import init,Get_Neighborhood,Get_mapping_reward,Get_detailed_data,find_start_task,get_sorted_dict,ActorCritic,Get_full_route_by_XY,Get_reward_by_pendTimes,Environment,check_if_Done,Actor,Critic
+from libs import init,Get_Neighborhood,Get_detailed_data,find_start_task,get_sorted_dict,ActorCritic,Get_full_route_by_XY,Get_reward_by_pendTimes,Environment,check_if_Done,Actor,Critic
 from queue import Queue
 from OnlineCompute1 import onlineTimeline
 import copy
@@ -153,7 +153,8 @@ def routeCompute(adj_matrix,num_of_tasks,execution,num_of_rows,MapResult):
     #print(task_graph)
     task.loadGraphByDict(task_graph,MapResult,fullRouteFromRL,[-1,-1],num_of_tasks)
     pendTimes=task.computeTime()
-    return Get_reward_by_pendTimes(pendTimes),task_graph
+    #return Get_reward_by_pendTimes(pendTimes),task_graph
+    return pendTimes,task_graph
 
 
     

test.py
@@ -13,7 +13,7 @@ from torch.distributions import Categorical
 
 import matplotlib.pyplot as plt
 import time
-from libs import init,Get_full_route_by_XY,Environment,check_if_Done,Critic,Get_detailed_data,Get_rand_computation_ability
+from libs import init,Get_full_route_by_XY,Environment,check_if_Done,Critic,Get_detailed_data,Get_rand_computation_ability2,Get_mapping_exe_time
 from queue import Queue
 
 import datetime
@@ -71,10 +71,21 @@ print("test tmp",tmp,type(tmp))
 log_prob=dist.log_prob(action)
 print(log_prob,log_prob.shape)
 """
-hyperperiod,num_of_tasks,edges,comp_cost=init('./task graph/N12_autocor.tgff')
+hyperperiod,num_of_tasks,edges,comp_cost=init('./task graph/N12_autocor_AIR1.tgff')
 adj_matrix,total_needSend,total_needReceive,execution=Get_detailed_data(num_of_tasks,edges,comp_cost)
+computation_ability=Get_rand_computation_ability2(num_of_rows=4)#2的指数级，4/8/16
+Tasks_position_current_solution={0: 4, 1: 6, 2: 6, 3: 6, 4: 11, 5: 6, 6: 6, 7: 2, 8: 13, 9: 8, 10: 1, 11: 5}
+PEs_task_current_solution=[]
+for i in range(0,4*4):
+    PEs_task_current_solution.append([])
+for i in Tasks_position_current_solution.keys():
+    PEs_task_current_solution[Tasks_position_current_solution[i]].append(i)
 print(num_of_tasks)
 print(adj_matrix)
+print(execution)
+print(computation_ability)
+ret=Get_mapping_exe_time(PEs_task_current_solution,Tasks_position_current_solution,computation_ability,4,execution)
+print(ret)
 
 
 